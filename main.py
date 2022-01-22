55bab566a5129e9db8e47793907eba43a2d6d384
AnimatorOfSouls <fantailfishy@gmail.com>
2022-01-22 19:27:05 +0000

    Reverted flask because it's useless

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from flask import Flask, render_template
-
 #import modules.deepgram
 from modules.analyse import *
 #import modules.translate
@@ -8,22 +6,10 @@ from modules.analyse import *
 # =======
 
 # EXAMPLE: running this should output '-0.8' and '-0.15'
-#text = "I really hate this product"
-#sentence = ["I really hate this product", "but it's also quite interesting to play with"]
-#print(analyse(text))
-#print(analyse2(sentence))
+text = "I really hate this product"
+sentence = ["I really hate this product", "but it's also quite interesting to play with"]
+print(analyse(text))
+print(analyse2(sentence))
 # >>>>>>> 76be03e2bf8b9ebb2cde64f7b5e09881ebac0465
 
 
-app = Flask(__name__)
-
-@app.route("/")
-def translate():
-	return render_template("translate.html")
-	
-@app.route("/about")
-def about():
-	return render_template("about.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
