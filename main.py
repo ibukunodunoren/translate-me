2c87a8062bc4924c198e4d4d3c9b956c046bc300
AnimatorOfSouls <fantailfishy@gmail.com>
2022-01-22 13:59:18 +0000

    Finished analysis functions

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 #import modules.deepgram
-#import modules.analyse
+from modules.analyse import *
 #import modules.translate
+
+
+# EXAMPLE: running this should output '-0.8' and '-0.15'
+text = "I really hate this product"
+sentence = ["I really hate this product", "but it's also quite interesting to play with"]
+print(analyse(text))
+print(analyse2(sentence))
--- modules/analyse.py
+++ modules/analyse.py
@@ -1,2 +1,21 @@
-# Performs sentiment analysis on a message and returns the sentiment
-def analyse():
+from textblob import TextBlob
+
+# Performs sentiment analysis on a message and returns the sentiment rounded to 2dp
+def analyse(text):
+    return round(TextBlob(text).sentiment.polarity, 2)
+
+# input an array of text
+def analyse2(sentence):
+    polarity = 0
+    for slice in sentence:
+        polarity += TextBlob(slice).sentiment.polarity
+        
+    return round(polarity/len(sentence), 2)
+    
+text = "I really hate this product"
+sentence = ["I really hate this product", "but it's also quite interesting to play with"]
+
+pol = analyse(text)
+print(pol)
+pol = analyse2(sentence)
+print(pol)
